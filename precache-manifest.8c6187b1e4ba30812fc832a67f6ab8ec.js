213cd2329e67e909d333377e31c9633e3e8dfb4e
botang13 <herbowoprasetyo@gmail.com>
2019-05-08 21:09:13 +0700

    Portfolio of me

---
+++ precache-manifest.8c6187b1e4ba30812fc832a67f6ab8ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0711e93d278e62d1790c6c96ee05b51b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "827e3b3b5feba3224963",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "827e3b3b5feba3224963",
+    "url": "/static/js/main.69a6fd31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
